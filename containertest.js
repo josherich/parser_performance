49298f5372dfa895912c7e2935af7c6879940786
josh chen <josherichchen@gmail.com>
2022-06-17 21:29:32 -0400

    fix weakmap tests

---
+++ containertest.js
@@ -0,0 +1,189 @@
+/*
+    node --expose-gc containertest.js
+    js containertest.js
+     or open containertest.html in your browser.
+*/
+
+var OBJECT_COUNT   = 13000000;
+var STEP_SIZE      = 100000;
+var FULL_TEST_RUNS = 1;
+
+function TestObject (index) {
+    this.index = index;
+};
+
+function Case () {
+    this.map = null;
+    this.weakMap = null;
+    this.jsObj = null;
+
+    var timeStarts = Object.create(null);
+
+    this.time = function (key) {
+        timeStarts[key] = Date.now();
+    };
+
+    this.timeEnd = function (key) {
+        var ended = Date.now();
+        var started = timeStarts[key];
+
+        delete timeStarts[key];
+
+        if (typeof (started) !== "number")
+            throw new Error("Mismatched start/end");
+
+        var elapsed = ended - started;
+        this.log(key + ", " + elapsed.toFixed(0));
+    };
+
+    if (
+        (typeof (window) === "object") &&
+        (typeof (document) === "object") &&
+        document.getElementById("log")
+    ) {
+        var logTextarea = document.getElementById("log");
+        this.log = function () {
+            logTextarea.value += Array.prototype.join.call(arguments, " ") + "\n";
+        };
+    } else if (typeof (console) === "object") {
+        this.log = console.log.bind(console);
+        // this.time = console.time.bind(console);
+        // this.timeEnd = console.timeEnd.bind(console);
+    } else {
+        this.log = function () {
+            print.apply(null, arguments);
+        };
+    }
+
+    this.log("// Creating " + OBJECT_COUNT + " test object instances");
+
+    this.time("startup");
+
+    this.objects = new Array(OBJECT_COUNT);
+    for (var i = 0; i < OBJECT_COUNT; i++)
+        this.objects[i] = new TestObject(i);
+
+    this.timeEnd("startup");
+
+    if (typeof (gc) === "function") {
+        this.collectGarbage = gc;
+    } else {
+        this.log("No triggered garbage collection available; this may negatively impact results.");
+
+        this.collectGarbage = function noManualGcAvailable () {
+        };
+    }
+
+    this.collectGarbage();
+};
+
+Case.prototype.setUp = function () {
+    this.map = new Map();
+    this.weakMap = new WeakMap();
+    this.jsObj = Object.create(null);
+
+    this.collectGarbage();
+};
+
+Case.prototype.logTick = function (i, isEnd) {
+    var endpoint = Math.min(i + STEP_SIZE, OBJECT_COUNT) - 1;
+    // var text = "[" + String(i) + "-" + String(endpoint) + "]";
+    var text = String(i);
+    if (isEnd)
+        this.timeEnd(text);
+    else
+        this.time(text);
+};
+
+Case.prototype.runMap = function () {
+    this.setUp();
+
+    this.log("// new Map()");
+    this.time("js Map");
+
+    for (var i = 0; i < OBJECT_COUNT; i += STEP_SIZE) {
+        this.logTick(i, false);
+        for (var j = 0, n = Math.min(OBJECT_COUNT - i, STEP_SIZE); j < n; j++) {
+            var k = (i + j) | 0;
+
+            var obj = this.objects[k];
+            this.map.set(obj, k);
+        }
+        this.logTick(i, true);
+    }
+
+    this.timeEnd("js Map");
+
+    this.tearDown();
+};
+
+Case.prototype.runJsObject = function () {
+    this.setUp();
+
+    this.log("// Object.create(null)");
+    this.time("js object");
+
+    for (var i = 0; i < OBJECT_COUNT; i += STEP_SIZE) {
+        this.logTick(i, false);
+        for (var j = 0, n = Math.min(OBJECT_COUNT - i, STEP_SIZE); j < n; j++) {
+            var k = (i + j) | 0;
+
+            var obj = this.objects[k];
+            this.jsObj[obj.index] = k;
+        }
+        this.logTick(i, true);
+    }
+
+    this.timeEnd("js object");
+
+    this.tearDown();
+};
+
+Case.prototype.runWeakMap = function () {
+    this.setUp();
+
+    this.log("// new WeakMap()");
+    this.time("weakmap");
+
+    for (var i = 0; i < OBJECT_COUNT; i += STEP_SIZE) {
+        this.logTick(i, false);
+        for (var j = 0, n = Math.min(OBJECT_COUNT - i, STEP_SIZE); j < n; j++) {
+            var k = (i + j) | 0;
+
+            var obj = this.objects[k];
+            this.weakMap.set(obj, k);
+        }
+        this.logTick(i, true);
+    }
+
+    this.timeEnd("weakmap");
+
+    this.tearDown();
+};
+
+Case.prototype.tearDown = function () {
+    if (this.map.clear)
+        this.map.clear();
+
+    // *displeased noises*
+    if (this.weakMap.clear)
+        this.weakMap.clear();
+
+    this.map = null;
+    this.weakMap = null;
+    this.jsObj = null;
+
+    this.collectGarbage();
+};
+
+{
+    var c = new Case();
+
+    for (var i = 0; i < FULL_TEST_RUNS; i++) {
+        c.runJsObject();
+        c.runMap();
+        c.runWeakMap();
+    }
+
+    c.log("// done");
+}
--- weakmap.js
+++ weakmap.js
@@ -1,83 +1,42 @@
 const Benchmark = require("benchmark");
 const Table = require("cli-table");
 
-const suite = new Benchmark.Suite('WeakMap', { minSamples: 10 });
-const head = ["op", "Set", "Has", "Get", "Delete"];
-const table = new Table({
-  head,
-  style: {
-    head: ["bold"]
+class Position {
+  constructor(index) {
+    this.index = index;
+    this.indexs = String(index);
   }
-});
-
-suite.add('Set', WeakMapSet);
-
-suite.add('Has', WeakMapHas, {
-  onStart: WeakMapSetup,
-  onComplete: WeakMapTearDown
-});
-
-suite.add('Get', WeakMapGet, {
-  onStart: WeakMapSetup,
-  onComplete: WeakMapTearDown
-});
-
-suite.add('Delete', WeakMapDelete, {
-  onStart: WeakMapSetup,
-  onComplete: WeakMapTearDown
-});
-
-suite.add('prop set', propSet);
-suite.add('prop get', propGet, {
-  onStart: propSetup,
-  onComplete: propTeardown
-});
-
-const result = [suite.name];
-suite.on('cycle', function(event) {
-  {
-    const bench = event.target;
-    const factor = bench.hz < 100 ? 100 : 1;
-    const timeMs = bench.stats.mean * 1000;
-    const time = (timeMs < 10)? `${Math.round(timeMs*1000)/1000}ms` : `${Math.round(timeMs)}ms`;
-    const msg = `${Math.round((bench.hz * factor) /
-      (factor * 1000000))}m ops/sec ±${Math.round(bench.stats.rme * 100) /
-      100}% (${time})`;
-    result.push(msg);
-  }
-  global.gc();
-});
-
-console.log(`Running benchmark for ${suite.name} ...`);
-
-global.gc();
-suite.run({ async: false });
-global.gc(); // gc is disabled so ensure we run it
-
-table.push(result);
-console.log(table.toString());
+}
 
 var wm;
-var N = 10;
+var N = 3000000;
+var STEP_SIZE = 1;
 var keys = [];
-for (var i = 0; i < N * 2; i++) {
-  keys[i] = {};
+for (var i = 0; i < N; i++) {
+  keys[i] = new Position(i);
 }
 var poss = []
 
 /* ------------------------------------- */
 
-class Position {
-  constructor(index) {
-    this.index = index;
-  }
-}
 function WeakMapSetup() {
   wm = new WeakMap;
-  for (var i = 0; i < N; i++) {
-    wm.set(keys[i], i);
+  // for (var i = 0; i < N; i++) {
+  //   wm.set(keys[i], i);
+  // }
+  for (var i = 0; i < N; i += STEP_SIZE) {
+    // var t1 = Date.now();
+    for (var j = 0, n = Math.min(N - i, STEP_SIZE); j < n; j++) {
+      var k = (i + j) | 0;
+
+      var obj = keys[k];
+      wm.set(obj, k);
+    }
+    // var t2 = Date.now();
+    // console.log((t2-t1).toFixed(0) + 'ms');
   }
 }
+
 function WeakMapTearDown() {
   wm = null;
 }
@@ -142,3 +101,61 @@ function propGet() {
     }
   }
 }
+
+
+const suite = new Benchmark.Suite('WeakMap', { minSamples: 10 });
+const head = ["op", "Set", "Has", "Get", "Delete"];
+const table = new Table({
+  head,
+  style: {
+    head: ["bold"]
+  }
+});
+
+suite.add('Set', WeakMapSet);
+
+suite.add('Has', WeakMapHas, {
+  onStart: WeakMapSetup,
+  onComplete: WeakMapTearDown
+});
+
+suite.add('Get', WeakMapGet, {
+  onStart: WeakMapSetup,
+  onComplete: WeakMapTearDown
+});
+
+suite.add('Delete', WeakMapDelete, {
+  onStart: WeakMapSetup,
+  onComplete: WeakMapTearDown
+});
+
+suite.add('prop set', propSet);
+suite.add('prop get', propGet, {
+  onStart: propSetup,
+  onComplete: propTeardown
+});
+
+const result = [suite.name];
+suite.on('cycle', function(event) {
+  {
+    const bench = event.target;
+    const factor = bench.hz < 100 ? 100 : 1;
+    const timeMs = bench.stats.mean * 1000;
+    const time = (timeMs < 10)? `${Math.round(timeMs*1000)/1000}ms` : `${Math.round(timeMs)}ms`;
+    const msg = `${bench.hz} ops/sec ±${Math.round(bench.stats.rme * 100) /
+      100}% (${time})`;
+    result.push(msg);
+  }
+  global.gc();
+});
+
+console.log(`Running benchmark for ${suite.name} ...`);
+
+// WeakMapSetup()
+
+global.gc();
+suite.run({ async: false });
+global.gc(); // gc is disabled so ensure we run it
+
+table.push(result);
+console.log(table.toString());
